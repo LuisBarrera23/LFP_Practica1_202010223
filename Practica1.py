b4ec9892eb766430e3b727c793ab4145113c1fcc
Luis Angel Barrera Velásquez <luisbarrera5662@gmail.com>
2021-08-05 23:31:28 -0600

    Reportes por consola terminado

--- Practica1.py
+++ Practica1.py
@@ -8,6 +8,9 @@ notas=[]
 notas2=[]
 reportes=[]
 cantidad_alum=0
+promedio=0
+aprobados=0
+reprobados=0
 
 #FUNCION PARA GENERAR REPORTE DE LOS DATOS GUARDADOS AL LEER EL ARCHIVO
 def reportelectura():
@@ -17,11 +20,123 @@ def reportelectura():
     for i in range(cantidad_alum):
         print("El estudiante: ",alumnos[i]," tiene una nota de: ",notas[i])
 
+#Reporte ASC orden ascendente
+def mostrarascendente():
+    global alumnos2,notas2
+
+    for i in range(1,len(notas2)):
+        for j in range(0,len(notas2)-i):
+            if(notas2[j+1]<notas2[j]):
+                aux1=notas2[j]
+                aux2=alumnos2[j]
+                notas2[j]=notas2[j+1]
+                alumnos2[j]=alumnos2[j+1]
+                notas2[j+1]=aux1
+                alumnos2[j+1]=aux2
+    
+    print("-----Listado de Estudiantes segun nota de forma ascendente-----")
+    for i in range(len(alumnos2)):
+        print("Estudiante: ",alumnos2[i],"\t Nota: ",notas2[i])
+    print("\n")
+
+#Reporte DESC orden descendente    
+def mostrardescendente():
+    global alumnos2,notas2
+
+    for i in range(1,len(notas2)):
+        for j in range(0,len(notas2)-i):
+            if(notas2[j+1]>notas2[j]):
+                aux1=notas2[j]
+                aux2=alumnos2[j]
+                notas2[j]=notas2[j+1]
+                alumnos2[j]=alumnos2[j+1]
+                notas2[j+1]=aux1
+                alumnos2[j+1]=aux2
+    
+    print("-----Listado de Estudiantes segun nota de forma descendente-----")
+    for i in range(len(alumnos2)):
+        print("Estudiante: ",alumnos2[i],"\t Nota: ",notas2[i])
+    print("\n")
+
+#Reporte AVG calculo del promedio de notas de los estudiantes
+def mostrarpromedio():
+    global notas2,promedio
+    promedio=0
+    suma=0
+
+    for i in range(len(notas2)):
+        suma+=notas2[i]
+    promedio=round(suma/len(notas2),2)
+    print("-----\tEl promedio de los estudiantes del curso es: ",promedio,"\t-----")
+    print("\n")
+
+
+#Reporte MAX mostrar la nota mas alta
+def mostrarMax():
+    global alumnos2,notas2
+
+    for i in range(1,len(notas2)):
+        for j in range(0,len(notas2)-i):
+            if(notas2[j+1]>notas2[j]):
+                aux1=notas2[j]
+                aux2=alumnos2[j]
+                notas2[j]=notas2[j+1]
+                alumnos2[j]=alumnos2[j+1]
+                notas2[j+1]=aux1
+                alumnos2[j+1]=aux2
+    
+    print("-----\tLa nota maxima es del estudiante: ",alumnos2[0]," con ", notas2[0],"\t-----")
+    print("\n")
+
+
+#Reporte MIN mostrar la nota mas baja
+def mostrarMin():
+    global alumnos2,notas2
+
+    for i in range(1,len(notas2)):
+        for j in range(0,len(notas2)-i):
+            if(notas2[j+1]<notas2[j]):
+                aux1=notas2[j]
+                aux2=alumnos2[j]
+                notas2[j]=notas2[j+1]
+                alumnos2[j]=alumnos2[j+1]
+                notas2[j+1]=aux1
+                alumnos2[j+1]=aux2
+    
+    print("-----\tLa nota minima es del estudiante: ",alumnos2[0]," con ", notas2[0],"\t-----")
+    print("\n")
+
+#Reporte APR mostrar estudiantes aprobados
+def mostrarAprobados():
+    global notas2,aprobados
+    aprobados=0
+
+    for i in range(len(notas2)):
+        if notas2[i]>=61:
+            aprobados+=1
+    
+    print("-----\tEstudiantes aprobados en el curso: ",aprobados,"\t-----")
+    print("\n")
+
+
+#Reporte APR mostrar estudiantes reprobados
+def mostrarReprobados():
+    global notas2,reprobados
+    reprobados=0
+
+    for i in range(len(notas2)):
+        if notas2[i]<61:
+            reprobados+=1
+    
+    print("-----\tEstudiantes reprobados en el curso: ",reprobados,"\t-----")
+    print("\n")
+    
 
 def cargararchivo():
-    global curso,alumnos,alumnos2,notas,notas2,cantidad_alum
+    global curso,alumnos,alumnos2,notas,notas2,cantidad_alum,reportes
+    alumnos.clear(),alumnos2.clear(),notas.clear(),notas2.clear(),reportes.clear()
 
-    Tk().withdraw()
+    #Tk().withdraw()
     #GENERANDO VENTANA PARA ELEGIR ARCHIVO
     archivo=filedialog.askopenfile(
         title="Selecciona un archivo en formato LFP",
@@ -71,22 +186,10 @@ def cargararchivo():
     #GENERAR REPORTE DE LA LECTURA
     reportelectura()
 
-    #PRUEBAS DE ORDENAMIENTO
 
-    for i in range(1,len(notas2)):
-        for j in range(0,len(notas2)-i):
-            if(notas2[j+1]>notas2[j]):
-                aux1=notas2[j]
-                aux2=alumnos2[j]
-                notas2[j]=notas2[j+1]
-                alumnos2[j]=alumnos2[j+1]
-                notas2[j+1]=aux1
-                alumnos2[j+1]=aux2
-    #print(alumnos2)
-    #print(notas2)
 
 def menu():
-    global opcion
+    global opcion,reportes,curso,cantidad_alum
     while True:
         print('--------------------MENU PRINCIPAL--------------------')
         print('1. Cargar archivo')
@@ -99,7 +202,23 @@ def menu():
             if opcion==1:
                 cargararchivo()
             elif opcion==2:
-                print("Mostrar reporte")
+                print("Curso: ",curso)
+                print("Estudiantes inscritos: ",cantidad_alum)
+                for i in range(len(reportes)):
+                    if reportes[i]=="ASC":
+                        mostrarascendente()
+                    elif reportes[i]=="DESC":
+                        mostrardescendente()
+                    elif reportes[i]=="AVG":
+                        mostrarpromedio()
+                    elif reportes[i]=="MIN":
+                        mostrarMin()
+                    elif reportes[i]=="MAX":
+                        mostrarMax()
+                    elif reportes[i]=="APR":
+                        mostrarAprobados()
+                    elif reportes[i]=="REP":
+                        mostrarReprobados()
             elif opcion==3:
                 print("Exportar Reporte")
             elif opcion==4:
